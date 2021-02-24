24b2c2886aca551e3cf57d17a6ef1d4335001bff
Agung Cahya Ilhami <agung@doku.com>
2021-02-24 15:26:16 +0700

    initial project

---
+++ src/main/java/com/spring/project/flickrimagecollector/FlickrImageCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.spring.project.flickrimagecollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlickrImageCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlickrImageCollectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/project/flickrimagecollector/FlickrImageCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.project.flickrimagecollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlickrImageCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
